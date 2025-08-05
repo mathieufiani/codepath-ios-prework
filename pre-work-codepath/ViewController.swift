de060238a40e179fc4591d86163dcb60c280968d
mathieufiani <mathieu.fiani@gmail.com>
2025-08-05 23:17:47 +0200

    Create an app that change background and text color randomly on button click

--- pre-work-codepath/ViewController.swift
+++ pre-work-codepath/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myName: UILabel!
+    @IBOutlet weak var myUniversity: UILabel!
+    @IBOutlet weak var myJob: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myImage.image = UIImage(named: "myImage")
     }
 
-
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    @IBAction func changeTextColor(_ sender: UILabel) {
+        let randomColor = changeColor()
+        myName.textColor = randomColor
+        myJob.textColor = randomColor
+        myUniversity.textColor = randomColor
+    }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
 }
 
